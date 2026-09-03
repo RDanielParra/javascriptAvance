ab5961637544af4801ce1b7158745f3e6b22cd78
Reynaldo Daniel <reyesparrareynaldodaniel@gmail.com>
2026-09-03 15:08:22 -0500

    arreglos finales

--- practica1JavascriptRepaso/src/main.ts
+++ practica1JavascriptRepaso/src/main.ts
@@ -1,60 +1,10 @@
 import './style.css'
-import { getProductos } from './services/productService'
+import { candidatosPromocion, getProductos, precioDescuentos, productosCategoria, valorInventario } from './services/productService'
 import type Product from './models/product'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 
 `
-async function candidatosPromocion(){
-  let products = await getProductos()
-
-  let productosTratados = products.filter(e => e.rating >= 4.5)
-  productosTratados = products.filter(e => e.stock > 10)
-  let productosFormateados = productosTratados.map(e => ({
-    Nombre: e.title,
-    Precio: e.price,
-    Stock: e.stock,
-    Rating: e.rating,
-  }))
-  console.log(`=== PRODUCTOS CANDIDATOS PARA PROMOCION === \n`)
-  console.log(productosFormateados)
-}
-
-async function valorInventario() {
-  let products = await getProductos()
-  let valorTotalIventario: number = 0
-  products.forEach(e => {
-    valorTotalIventario += e.price * e.stock
-  })
-  console.log(`=== VALOR TOTAL DEL INVENTARIO === \n`)
-  console.log(`Valor total del Inventario: ${valorTotalIventario.toFixed(2)}`)
-}
-
-async function precioDescuentos() {
-  let products = await getProductos()
-  let productosConDescuento = products.map(e => ({
-    NombreDelProducto: e.title,
-    PrecioOriginal: e.price,
-    PorcentajeDescuento: e.discountPercentage,
-    PrecioFinal: (e.price - (e.price * e.discountPercentage / 100)).toFixed(2)
-  }))
-
-  console.log(`=== PRODUCTOS CANDIDATOS PARA PROMOCION === \n`)
-  console.log(productosConDescuento)
-}
-
-async function productosCategoria() {
-  let products = await getProductos()
-
-  const conteoPorCategoria = products.reduce((acc, producto) => {
-  const cat = producto.category;
-  acc[cat] = (acc[cat] || 0) + 1;
-  return acc;
-  }, {} as Record<string, number>);
-
-  console.log(`=== PRODUCTOS POR CATEGORIA === \n`)
-  console.log(conteoPorCategoria);
-}
 
 candidatosPromocion()
 valorInventario()
--- practica1JavascriptRepaso/src/services/productService.ts
+++ practica1JavascriptRepaso/src/services/productService.ts
@@ -4,15 +4,66 @@ export async function getProductos(): Promise<Product[]> {
   const res = await fetch('https://dummyjson.com/products');
   const data = await res.json();
 
-  const productosFiltrados: Product[] = data.products.map((item: any) => ({
-    title: item.title,
-    price: item.price,
-    discountPercentage: item.discountPercentage,
-    description: item.description,
-    category: item.category,
-    stock: item.stock,
-    rating: item.rating,
+  const productosFiltrados: Product[] = data.products.map((e: any) => ({
+    title: e.title,
+    price: e.price,
+    discountPercentage: e.discountPercentage,
+    description: e.description,
+    category: e.category,
+    stock: e.stock,
+    rating: e.rating,
   }));
 
   return productosFiltrados;
+}
+
+export async function candidatosPromocion(){
+  let products = await getProductos()
+
+  let productosTratados = products.filter(e => e.rating >= 4.5)
+  productosTratados = products.filter(e => e.stock > 10)
+  let productosFormateados = productosTratados.map(e => ({
+    Nombre: e.title,
+    Precio: e.price,
+    Stock: e.stock,
+    Rating: e.rating,
+  }))
+  console.log(`=== PRODUCTOS CANDIDATOS PARA PROMOCION === \n`)
+  console.log(productosFormateados)
+}
+
+export async function valorInventario() {
+  let products = await getProductos()
+  let valorTotalIventario: number = 0
+  products.forEach(e => {
+    valorTotalIventario += e.price * e.stock
+  })
+  console.log(`=== VALOR TOTAL DEL INVENTARIO === \n`)
+  console.log(`Valor total del Inventario: ${valorTotalIventario.toFixed(2)}`)
+}
+
+export async function precioDescuentos() {
+  let products = await getProductos()
+  let productosConDescuento = products.map(e => ({
+    NombreDelProducto: e.title,
+    PrecioOriginal: e.price,
+    PorcentajeDescuento: e.discountPercentage,
+    PrecioFinal: (e.price - (e.price * e.discountPercentage / 100)).toFixed(2)
+  }))
+
+  console.log(`=== PRODUCTOS CANDIDATOS PARA PROMOCION === \n`)
+  console.log(productosConDescuento)
+}
+
+export async function productosCategoria() {
+  let products = await getProductos()
+
+  const conteoPorCategoria = products.reduce((acc, producto) => {
+  const cat = producto.category;
+  acc[cat] = (acc[cat] || 0) + 1;
+  return acc;
+  }, {} as Record<string, number>);
+
+  console.log(`=== PRODUCTOS POR CATEGORIA === \n`)
+  console.log(conteoPorCategoria);
 }
